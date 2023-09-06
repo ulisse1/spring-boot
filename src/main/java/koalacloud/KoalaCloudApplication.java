d6b65549760df4ce060bfb52abe30d2d6622d2c9
ulisse1 <ulisedumitrescu@yahoo.com>
2023-09-06 22:06:57 +0300

    Initial commit

---
+++ src/main/java/koalacloud/KoalaCloudApplication.java
@@ -0,0 +1,13 @@
+package koalacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoalaCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoalaCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koalacloud/KoalaCloudApplicationTests.java
@@ -0,0 +1,28 @@
+package koalacloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest
+class KoalaCloudApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
+	@Test
+	void contextLoads() {
+	}
+	@Test
+	void a() {
+	}
+	@Test
+	void asd() {
+	}
+}
